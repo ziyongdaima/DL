f136ffbf6844e3098b0b31a22e7f5e8564e7b0f6
ziyongdaima <153406854+ziyongdaima@users.noreply.github.com>
2026-09-09 23:20:07 +0800

    Update 喜福短剧.py

--- py/喜福短剧.py
+++ py/喜福短剧.py
@@ -50,7 +50,7 @@ headers = {
 class Spider(Spider):
 
     def getName(self):
-        return "🌷"
+        return ""
 
     def init(self, extend):
         pass
@@ -73,7 +73,7 @@ class Spider(Spider):
             for vod in data['data']['categories']:
                 category_info = {
                     "type_id": vod['id'],
-                    "type_name": f"🌷{vod['name']}"
+                    "type_name": f"{vod['name']}"
                                 }
                 categories.append(category_info)
             return categories
@@ -102,7 +102,7 @@ class Spider(Spider):
                     "vod_id": f"{vod['albumId']}@{vod['total']}",
                     "vod_name": vod['title'],
                     "vod_pic": vod['coverUrl'],
-                    "vod_remarks": f"🌷共{vod.get('total', '暂无备注')}集"
+                    "vod_remarks": f"共{vod.get('total', '暂无备注')}集"
                              }
                 video_list.append(video_info)
             return video_list
